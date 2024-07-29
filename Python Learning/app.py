cc905203f660327c5f27e3d14d1205def932826a
RehamElaraby <rehamelaraby2272@gmail.com>
2024-07-29 20:58:06 +0200

    python learning

---
+++ Python Learning/app.py
@@ -0,0 +1,20 @@
+################################################################
+#variables 
+charachter_name = "mourad"  
+age = "50"
+isRight = True 
+
+print("the name is " + charachter_name + " GOOD!") 
+print("his age is " + age + " so yong!")  
+
+#########################################################################
+#Strings used in plain text &  strings 
+
+print("rehaaaaaaaam") 
+#i can didive sentense by ((( \n )))) to be on multiple lines it's called special character
+# use ((((  \" )))) to display " and (((( \\ ))))  to display \ and (((  \t ))) to display 8 spaces
+print("rehaaa \n aaaaam") 
+# use (((( + )))) to concatenate 
+text = "hi this concatenation"
+print(text + "rehaaa \n aaaaam") 
+# فيديو 05 الدقيقة 2:56 
